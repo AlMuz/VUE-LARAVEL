9882211346a015782767e81ef3467724ff256da9
AlMuz <almuz@inbox.lv>
2019-05-22 22:48:23 +0300

    vue configuration 2

---
+++ resources/js/routes/routes.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter);
+
+const Router = new VueRouter({
+  mode: 'history',
+  routes: [
+    
+  ]
+})
+
+export default Router;
---
+++ resources/js/views/app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <div class="container">
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'app',
+  components: {
+  }
+};
+</script>
