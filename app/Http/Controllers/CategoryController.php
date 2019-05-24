e8fc27b455d91d8d5bba5066329899bbe6c14a77
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-05-24 17:02:34 +0300

    not completed

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller {
 
+    public function getAllUserCategories(Request $request, Category $category) {
+
+        // $token = $request->bearerToken();
+        // $category
+        //     ->leftJoin('users', 'users.id', '=', 'categories.user_id')
+        //     ->where('users.api_token', $token)
+        //     ->get();
+        // var_dump($category);
+        // return $category;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,23 @@ use App\User;
 
 Route::post('register', 'Auth\RegisterController@register');
 Route::post('login', 'Auth\LoginController@login');
+
+
+// Route::get('notes', function() {
+//
+//     return Note::all();
+// });
+
+// Route::get('notes/{id}', function($id) {
+//
+//     return Note::where('user_id', $id)->get();
+// });
+
+Route::middleware('auth:api')
+    ->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('categories', 'CategoryController@getAllUserCategories');
+});
