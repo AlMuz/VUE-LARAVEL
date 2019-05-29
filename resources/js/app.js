c5b7064b4ea8e1e05bed081f6862245001ef26aa
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-05-29 11:11:09 +0300

    importing libraries and storage added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,21 @@
 import './bootstrap'
 import Vue from 'vue'
+import VueNoty from 'vuejs-noty'
+import wysiwyg from "vue-wysiwyg"
+import 'vue-wysiwyg/dist/vueWysiwyg.css';
+import 'vuejs-noty/dist/vuejs-noty.css'
 
 import Routes from '@/js/routes/routes'
 
 import App from '@/js/views/app.vue'
 
+import store from './store'
+
+Vue.use(VueNoty);
+Vue.use(wysiwyg, {});
+
 const app = new Vue({
+  store,
   el: '#app',
   router: Routes,
   render: h => h(App)
